1c72c98faa742ae856aa4f3ed60456623367db25
Ryan McAlpine <mcalpine06@outlook.com>
2022-12-15 22:06:31 +1100

    Messing around with making blank windows and buttons

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use fltk::{app, button::Button, prelude::*, window::Window, frame::Frame};
+
 fn main() {
-    println!("Hello, world!");
+    hello_titlebar();
+    hello_titlebar1();
+}
+
+fn hello_titlebar1() {
+    let app = app::App::default();
+
+    let mut window = Window::new(100, 100, 400, 300, "Hello 2");
+    let mut frame = Frame::new(0, 0, 400, 200, "");
+    let mut button = Button::new(160, 210, 80, 40, "Button!");
+
+    window.end();
+    window.show();
+
+    button.set_callback(move |_| frame.set_label("Hello World"));
+    app.run().unwrap();
+}
+
+fn hello_titlebar() {
+    let app = app::App::default();
+
+    let mut window  = Window::new(1000, 100, 400, 300, "Hello FTLK");
+
+    window.end();
+    window.show();
+    app.run().unwrap();
 }
